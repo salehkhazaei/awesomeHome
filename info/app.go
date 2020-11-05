20fef7ad69f52d9c9090c0f7a6c6d7c2cf4cd9d3
Saleh Khazaei <saleh.khazaei@gmail.com>
2020-11-05 10:42:22 +0330

    feat(awesomeHome): Adds IP to app info

--- info/app.go
+++ info/app.go
@@ -3,6 +3,7 @@ package info
 import (
 	"encoding/json"
 	"fmt"
+	"ir.skhf/awesomeHome/utils"
 )
 
 var Major int = 1
@@ -10,10 +11,14 @@ var Minor int = 0
 
 type AppInfo struct {
 	Version string
+	IP      string
 }
 
 func Info() (string, error) {
-	appInfo := AppInfo{Version: Version()}
+	appInfo := AppInfo{
+		Version: Version(),
+		IP:      utils.GetOutboundIP().String(),
+	}
 
 	appInfoJson, err := json.Marshal(appInfo)
 	if err != nil {
