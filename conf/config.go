99916a94f5ea5511195f25e6c0df51320672f586
Saleh Khazaei <saleh.khazaei@gmail.com>
2020-11-05 15:49:53 +0330

    feat(awesomeHome): Adds config

---
+++ conf/config.go
@@ -0,0 +1,72 @@
+package conf
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type AwesomeHomeConfig struct {
+	ConfYaml
+	defaultConf []byte
+}
+
+type ConfYaml struct {
+	BroadcastPort int
+}
+
+func NewAwesomeHomeConfig(serviceName string, path string) *AwesomeHomeConfig {
+	conf := &AwesomeHomeConfig{defaultConf: []byte(`
+broadcast_port: 60504
+`)}
+	err := conf.loadConf(serviceName, path)
+	if err != nil {
+		log.Fatalf("Load yaml config file error: '%v'", err)
+		return nil
+	}
+	return conf
+}
+
+// LoadConf load config from file and read in environment variables that match
+func (conf *AwesomeHomeConfig) loadConf(serviceName string, path string) error {
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv() // read in environment variables that match
+	viper.SetEnvPrefix(serviceName)
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if path != "" {
+		content, err := ioutil.ReadFile(path)
+
+		if err != nil {
+			log.Errorf("File does not exist : %s", path)
+			return err
+		}
+
+		if err := viper.ReadConfig(bytes.NewBuffer(content)); err != nil {
+			return err
+		}
+	} else {
+		// Search config in home directory with name ".pkg" (without extension).
+		viper.AddConfigPath("..")
+		viper.AddConfigPath(".")
+		viper.AddConfigPath("./volumes/conf/")
+		viper.SetConfigName("config")
+
+		// If a config file is found, read it in.
+		if err := viper.ReadInConfig(); err == nil {
+			fmt.Println("Using config file:", viper.ConfigFileUsed())
+		} else {
+			// load default config
+			if err := viper.ReadConfig(bytes.NewBuffer(conf.defaultConf)); err != nil {
+				return err
+			}
+		}
+	}
+
+	conf.BroadcastPort = viper.GetInt("broadcast_port")
+	return nil
+}
