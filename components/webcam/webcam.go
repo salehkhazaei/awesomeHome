436361cfe8b9356f22458353f658c1a73f5db183
Saleh Khazaei <saleh.khazaei@gmail.com>
2020-11-12 16:44:15 +0330

    fix(awesomeHome): Separates windows and linux builds for webcam

--- components/webcam/webcam.go
+++ components/webcam/webcam.go
@@ -2,8 +2,6 @@ package webcam
 
 import (
 	"bytes"
-	"fmt"
-	"github.com/blackjack/webcam"
 	"log"
 	"math/rand"
 	"mime/multipart"
@@ -28,54 +26,6 @@ func (s *WebcamService) Init(dev string, fmtstr string, szstr string, fps bool) 
 	return s.OpenCamera(dev, fmtstr, szstr, fps)
 }
 
-func (s *WebcamService) HandleFrameRequests(cam *webcam.Webcam, format webcam.PixelFormat, w, h uint32) {
-	defer cam.Close()
-
-	var stopCount = 0
-	for {
-		if len(s.imageChannelMap) == 0 {
-			// no requests
-			time.Sleep(500 * time.Millisecond)
-			stopCount++
-
-			if stopCount == 4 {
-				err := s.StopCamera(cam)
-				if err != nil {
-					fmt.Printf("error in stopping camera: %v\n", err)
-					stopCount--
-					continue
-				}
-			}
-			continue
-		}
-
-		if stopCount > 0 {
-			err := s.ResumeCamera(cam)
-			if err != nil {
-				fmt.Printf("error in resuming camera: %v\n", err)
-				continue
-			}
-
-			stopCount = 0
-		}
-
-		fmt.Printf("sending frame for %d clients\n", len(s.imageChannelMap))
-
-		imgBuf, err := s.ReadImageFromCamera(cam, format, w, h)
-		if err != nil {
-			fmt.Printf("error in reading image from webcam: %v\n", err)
-			continue
-		}
-
-		for _, channel := range s.imageChannelMap {
-			select {
-			case channel <- imgBuf:
-			default:
-			}
-		}
-	}
-}
-
 func (s *WebcamService) HandleHttp(w http.ResponseWriter, r *http.Request) {
 	log.Println("connect from", r.RemoteAddr, r.URL)
 	startTime := time.Now()
@@ -97,7 +47,7 @@ func (s *WebcamService) HandleHttp(w http.ResponseWriter, r *http.Request) {
 	multipartWriter := multipart.NewWriter(w)
 	multipartWriter.SetBoundary(boundary)
 	for {
-		if time.Now().After(startTime.Add(30 * time.Second)) {
+		if time.Now().After(startTime.Add(60 * time.Second)) {
 			break
 		}
 
---
+++ components/webcam/webcam_windows.go
@@ -0,0 +1,8 @@
+// +build windows
+
+package webcam
+
+func (s *WebcamService) OpenCamera(dev string, fmtstr string, szstr string, fps bool) error {
+	//TODO-Saleh Implement this!
+	return nil
+}
